49e7924697b757f23ae9006aa1a8e904eaaf663e
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2024-04-17 23:10:51 -0300

    chore: configuraçao do build do projeto

--- src/test/java/io/github/wesleyosantos91/algafoodapi/ApplicationTests.java
+++ src/test/java/io/github/wesleyosantos91/algafoodapi/ApplicationTests.java
@@ -1,10 +1,11 @@
 package io.github.wesleyosantos91.algafoodapi;
 
+import io.github.wesleyosantos91.algafoodapi.testcontainer.ContainerBaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ApplicationTests extends ContainerBaseTest {
 
 	@Test
 	void contextLoads() {
